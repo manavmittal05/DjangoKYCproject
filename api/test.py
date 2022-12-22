c2c9ef60f91207647ac8d0139d85ded928fe20f9
Manav Mittal <65654441+manavmittal05@users.noreply.github.com>
2022-12-23 01:31:36 +0530

    sqlite3

---
+++ api/test.py
@@ -0,0 +1,17 @@
+from passporteye import read_mrz
+# Process image
+mrz = read_mrz("/home/manav/Desktop/Passports/P3.jpg")
+
+# Obtain image
+print(mrz)
+mrz_data = mrz.to_dict()
+
+print('Nationality :'+ mrz_data['nationality'])
+print('Given Name :'+ mrz_data['names'])
+print('Surname :'+ mrz_data['surname'])
+print('Passport type :'+ mrz_data['type'])
+print('Date of birth :'+ mrz_data['date_of_birth'])
+print('ID Number :'+ mrz_data['personal_number'])
+print('Gender :'+mrz_data['sex'])
+print('Expiration date :'+ mrz_data['expiration_date'])
+print(mrz_data,file=open('passportdata.csv',"a"))
